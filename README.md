614ccbaa128a5a5417c3fc7f4ae37f90c95768e4
Ziga Mahkovec <ziga.mahkovec@gmail.com>
2023-06-23 16:22:51 +0200

    Download energy data too

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 ## Introduction
 
-This script will download your entire history of Tesla Solar power data: solar/battery/grid power
-data in 5 minute intervals.  The script is using the [unofficial Tesla API](https://tesla-api.timdorr.com/)
-and [TeslaPy](https://github.com/tdorssers/TeslaPy) library.  Data is stored in CSV files by day.
+This script will download your entire history of Tesla Solar power and energy data:
+solar/battery/grid power data in 5 minute intervals and daily totals for solar/home/battery/grid
+energy.  The script is using the [unofficial Tesla API](https://tesla-api.timdorr.com/)
+and [TeslaPy](https://github.com/tdorssers/TeslaPy) library.  Data is stored in CSV files by day for
+power and a single `energy.csv` file for energy.
 
 ## Installation
 
@@ -35,14 +37,16 @@ locally so you can rerun the script if needed.
 Data will start downloading to the `download` directory.  Starting with today and going back in time
 all the way to the Tesla system's installation date.
 
-Downloads take ~3.5 seconds per day (~20 minutes per year of data).  This is mostly due to delays
-used to slow down the rate of API requests.  You may interrupt and restart the process -- any CSV
-files that already exist will be skipped during the next run.
+Power data downloads take ~1.5 seconds per day (~10 minutes per year of data).  This is mostly due
+to delays used to slow down the rate of API requests.  You may interrupt and restart the process
+-- any CSV files that already exist will be skipped during the next run.
+
+Energy downloads are faster (less than 30s per year) so they're done in entirety every time.
 
 
 ## Data
 
-The data is formatted as follows:
+Power data is formatted as follows:
 `download/<site_id>/2022-05-23.csv`
 ```CSV
 timestamp,solar_power,battery_power,grid_power,grid_services_power,generator_power,load_power
@@ -61,3 +65,13 @@ timestamp,solar_power,battery_power,grid_power,grid_services_power,generator_pow
 - All power values are in Watts.
 - load_power is simply a sum of solar+battery+grid+generator power and is what is shown as "house" load in the Tesla app.  (Note: this value is not included in API responses since it can be easily derived.)
 - grid_services_power and generator_power will likely be 0 and can be ignored.
+
+Energy data:
+`download/<site_id>/energy.csv`
+```CSV
+timestamp,solar_energy_exported,generator_energy_exported,grid_energy_imported,grid_services_energy_imported,grid_services_energy_exported,grid_energy_exported_from_solar,grid_energy_exported_from_generator,grid_energy_exported_from_battery,battery_energy_exported,battery_energy_imported_from_grid,battery_energy_imported_from_solar,battery_energy_imported_from_generator,consumer_energy_imported_from_grid,consumer_energy_imported_from_solar,consumer_energy_imported_from_battery,consumer_energy_imported_from_generator
+2022-03-20 01:00:00,19900,0,16020.54954135904,0,0,0,0,31.6434716614458,6920,373.8292054498161,12196.170794550184,0,15646.720335909224,7703.829205449816,6888.356528338554,0
+2022-03-21 01:00:00,18880,0,7672.932049195355,0,0,0,0,49.019242481099354,12540,274.12727761318456,9405.872722386815,0,7398.80477158217,9474.127277613185,12490.9807575189,0
+2022-03-22 01:00:00,18510,0,32628.105420033753,0,0,0,0,38.805653165723925,8180,365.48287811766204,12714.517121882338,0,32262.62254191609,5795.482878117662,8141.194346834276,0
+[...]
+```
--- tesla_solar_download.py
+++ tesla_solar_download.py
@@ -98,7 +98,7 @@ def _download_data(tesla, site_id, debug=False):
         if partial_day or not os.path.exists(_get_csv_name(date, site_id)):
             print(date.isoformat())
             _download_day(tesla, site_id, timezone, date, partial_day=partial_day)
-            time.sleep(3)
+            time.sleep(1)
         date -= timedelta(days=1)
         partial_day = False
         # Re-localize the date based on the timezone.  This is important because we maybe have
@@ -106,8 +106,82 @@ def _download_data(tesla, site_id, debug=False):
         date = pytz.timezone(timezone).localize(date.replace(tzinfo=None))
 
 
+def _get_energy_csv_name(site_id):
+    return f'download/{site_id}/energy.csv'
+
+
+def _write_energy_csv(timeseries, site_id):
+    if not timeseries:
+        raise ValueError('No timeseries')
+
+    csv_filename = _get_energy_csv_name(site_id)
+    os.makedirs(os.path.dirname(csv_filename), exist_ok=True)
+    file_exists = os.path.exists(csv_filename)
+    fieldnames = list(timeseries[0].keys())
+    with open(csv_filename, 'a') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        if not file_exists:
+            writer.writeheader()
+        for ts in timeseries:
+            ts['timestamp'] = parse(ts['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
+            writer.writerow(ts)
+
+
+@retry(tries=2, delay=5)
+def _download_energy_month(tesla, site_id, timezone, start_date, end_date):
+    print(start_date.isoformat(), end_date.isoformat())
+    response = tesla.api(
+        'CALENDAR_HISTORY_DATA',
+        path_vars={'site_id': site_id},
+        kind='energy',
+        period='month',
+        start_date=start_date.isoformat(),
+        end_date=end_date.isoformat(),
+        time_zone=timezone,
+        fill_telemetry=0,
+    )['response']
+
+    return response['time_series']
+
+
+def _download_energy_data(tesla, site_id, debug=False):
+    site_config = tesla.api('SITE_CONFIG', path_vars={'site_id': site_id})['response']
+    installation_date = parse(site_config['installation_date'])
+    timezone = site_config['installation_time_zone']
+
+    now = datetime.now(pytz.timezone(timezone)).replace(microsecond=0)
+    start_date = now.replace(hour=0, minute=0, second=0)
+    end_date = now.replace(hour=23, minute=59, second=59)
+
+    csv_name = _get_energy_csv_name(site_id)
+    if os.path.exists(csv_name):
+        os.remove(csv_name)
+    # Beginning of the month.
+    start_date = start_date - timedelta(days=start_date.day - 1)
+    if debug:
+        print(f'Timezone: {timezone}')
+        print(f'Start date: {start_date}')
+
+    timeseries = []
+    while end_date > installation_date:
+        timeseries.extend(
+            _download_energy_month(tesla, site_id, timezone, start_date, end_date)
+        )
+        time.sleep(1)
+        end_date = start_date - timedelta(seconds=1)
+        start_date = end_date.replace(hour=0, minute=0, second=0) - timedelta(
+            days=end_date.day - 1
+        )
+        start_date = pytz.timezone(timezone).localize(start_date.replace(tzinfo=None))
+
+    timeseries.sort(key=lambda x: x['timestamp'])
+    _write_energy_csv(timeseries, site_id)
+
+
 def _delete_partial_files(site_id):
     dir = os.path.join('download', str(site_id))
+    if not os.path.exists(dir):
+        return
     for fname in os.listdir(dir):
         if '.partial.csv' in fname:
             os.remove(os.path.join(dir, fname))
@@ -142,6 +216,7 @@ def main():
             print(f'Downloading data for {resource_type} site ***{str(site_id)[-4:]}')
             _delete_partial_files(site_id)
             _download_data(tesla, site_id, debug=args.debug)
+            _download_energy_data(tesla, site_id, debug=args.debug)
 
 
 if __name__ == '__main__':
