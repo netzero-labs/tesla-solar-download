9642771657197b3e7000d76385a4054c41ea4377
Ziga Mahkovec <ziga.mahkovec@gmail.com>
2024-01-26 10:12:02 -0800

    Fixes: get the first valid energy_site_id, pin TLS version

--- tools/fleet-api.sh
+++ tools/fleet-api.sh
@@ -19,13 +19,14 @@ if [ ! -f $TESLA_API_TOKEN_FILE ]; then
     exit 1
 fi
 
+CURL_CMD="curl -s --tlsv1.2 --tls-max 1.2"
 
 if [ "$1" == "--refresh" ]; then
     # Refresh tokens.
     NETZERO_CLIENT_ID="b7579e1434fe-4e23-aaee-faa4bcdc90d0"
     TESLA_API_REFRESH_TOKEN=$(jq -r .refresh_token $TESLA_API_TOKEN_FILE)
 
-    curl -s -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=refresh_token' \
+    $CURL_CMD -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'grant_type=refresh_token' \
         --data-urlencode "client_id=$NETZERO_CLIENT_ID" \
         --data-urlencode "refresh_token=$TESLA_API_REFRESH_TOKEN" \
         https://auth.tesla.com/oauth2/v3/token | jq . > $TESLA_API_TOKEN_FILE
@@ -36,15 +37,15 @@ fi
 TESLA_API_TOKEN=$(jq -r .access_token $TESLA_API_TOKEN_FILE)
 
 # Get the correct region for your site.
-TESLA_BASE_URL=$(curl -s -H "Authorization: Bearer $TESLA_API_TOKEN" https://fleet-api.prd.na.vn.cloud.tesla.com/api/1/users/region | jq -r .response.fleet_api_base_url)
+TESLA_BASE_URL=$($CURL_CMD -H "Authorization: Bearer $TESLA_API_TOKEN" https://fleet-api.prd.na.vn.cloud.tesla.com/api/1/users/region | jq -r .response.fleet_api_base_url)
 echo "Tesla base url: $TESLA_BASE_URL"
 
 # Get the energy site id.
-TESLA_SITE_ID=$(curl -s -H "Authorization: Bearer $TESLA_API_TOKEN" $TESLA_BASE_URL/api/1/products | jq -r .response[0].energy_site_id)
+TESLA_SITE_ID=$($CURL_CMD -H "Authorization: Bearer $TESLA_API_TOKEN" $TESLA_BASE_URL/api/1/products | jq .response[].energy_site_id | grep -v null | head -n1)
 echo "Tesla energy site id: $TESLA_SITE_ID"
 
 # Get site info.
-curl -s -H "Authorization: Bearer $TESLA_API_TOKEN" $TESLA_BASE_URL/api/1/energy_sites/$TESLA_SITE_ID/site_info | jq
+$CURL_CMD -H "Authorization: Bearer $TESLA_API_TOKEN" $TESLA_BASE_URL/api/1/energy_sites/$TESLA_SITE_ID/site_info | jq
 
 # To set backup reserve to 50%:
-#curl -s -H "X-Tesla-User-Agent: TeslaApp/4.29.0" -H "Authorization: Bearer $TESLA_API_TOKEN" --json '{"backup_reserve_percent": 50}' $TESLA_BASE_URL/api/1/energy_sites/$TESLA_SITE_ID/backup
+#$CURL_CMD -H "X-Tesla-User-Agent: TeslaApp/4.29.0" -H "Authorization: Bearer $TESLA_API_TOKEN" --json '{"backup_reserve_percent": 50}' $TESLA_BASE_URL/api/1/energy_sites/$TESLA_SITE_ID/backup
